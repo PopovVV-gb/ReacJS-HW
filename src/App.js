9bbfdc4bc4c1934b67f93198b751d6f3f6b86376
PopovVV-gb <83184318+PopovVV-gb@users.noreply.github.com>
2021-08-22 22:04:53 +0300

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import Message from './message';
+import MessageList from './components/messageList/messageList';
+import MessageInputForm from './components/messageInputForm/messageInputForm';
+import ChatList from './components/chatList/chatList';
+import {Paper, Grid, List } from '@material-ui/core/';
+import faker from 'faker'
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 
-const msgText = 'React first lesson';
+const chatList = Array.from({
+  length: 10}).map(() => (
+    {
+      id: faker.datatype.uuid(),
+      avatar: faker.image.avatar(),
+      name: faker.name.lastName(),}
+  ))
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0084FF",
+    },
+    secondary: {
+      main: "#FF5100",
+    },
+  },
+});
+
 
 function App(props) {
+  const [messageList, setMessageList] = useState([]);
+  const updateMessagelist = (author, text) => {
+    setMessageList((prevList) => prevList.concat({author, text}))
+  }
+
+  useEffect(() => {
+    if (messageList.length !== 0 && messageList[messageList.length-1].author === 'user1') {
+      const timeoutID = setTimeout(
+        () => updateMessagelist('autoreply', faker.lorem.words(20)),
+        Math.random()*2000
+      )
+      return () => clearTimeout(timeoutID)
+    }
+  }, [messageList]);
+
   return (
-    <div className="App">
-      <Message msgText={msgText}/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Grid className="grid-wrapper" container spacing = {3}>
+          <Grid item xs={3}>
+            <Paper>
+              <List>
+                <ChatList chatList={chatList}></ChatList>
+              </List>
+            </Paper>
+          </Grid>
+          <Grid item xs={9}>
+            <Paper>            
+              <MessageList messages={messageList} />
+            </Paper>
+          </Grid>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={9}>          
+            <MessageInputForm addMessage={updateMessagelist} />
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 }
 
